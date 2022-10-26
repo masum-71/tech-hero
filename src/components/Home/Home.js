640d2987a8c25b4d3789c4c0a5e56dbfcc685ea4
Masum Billah <masum.monpura@gmail.com>
2022-10-26 20:10:30 +0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
+  const courses = useLoaderData();
 
-    const courses = useLoaderData()
-
-    return (
-        <div>
-            <h1>home{courses.length}</h1>
-        </div>
-    );
+  return (
+    <div className="home-bg">
+      <div className="d-flex text-center text-white justify-content-center flex-column align-items-center h-100">
+        <h4>Enjoying with techHero, Best learning platform</h4>
+        <h1>Find your best career and get opportunity for your future</h1>
+        <p>World class learning for anyone, anyWhere for increasing Knowledge. let your creativity shine and start brighting your future today and impress your audiences</p>
+       <Link to='/courses'>
+       <button className="btn btn-primary">EXPLORE COURSES</button>
+       </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
